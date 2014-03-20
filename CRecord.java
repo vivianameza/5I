7dae1aa05dc3a04758f96d0487c4f6b51d25a1f7
damqfiansisc <danteamq@gmail.com>
2014-03-19 21:57:21 -0600

    Clase de Practica
    Clase para practica de push requests

---
+++ CRecord.java
@@ -0,0 +1,14 @@
+public class CRecord{
+private String clave;
+private String nombre;
+private String apellido;
+private Double peso;
+private Double estatura;
+
+/** Implementar constructores **/
+
+/** Implementar setters **/
+
+/** Implementar getters **/
+
+}
